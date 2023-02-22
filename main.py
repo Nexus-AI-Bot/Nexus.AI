093dcd27f78820be13250082344ab094b6826228
Sasha Bagrov <110257462+Fluffik3666@users.noreply.github.com>
2023-02-22 14:12:48 +0000

    modified lol

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from email.mime.text import MIMEText
 from pyowm.utils.config import get_default_config
 import webbrowser as wb
 import cool_functions as f
-
 sender = "helper.ai@fluffik.co.uk"
 password = "nxukvmybdpgymmjq"
 language = get_default_config()
@@ -36,20 +35,17 @@ class MyClient(discord.Client):
                 'Мои команды:\nПогода: /weather[место]\nРешение примеров: /calculate[пример]\n\Прикольный факт: /funfact\nПоиск в интернете: /search[запрос]\nИ генерация паролей: /password[кол-во символов]')
         if '/funfact' in msg:
             await message.channel.send(f.funfact())
-        find_hi_words = False
-        for item in hi:  # hello check
-            if msg.find(item) >= 0:
-                find_hi_words = True
         if '/calculate' in msg:
             await message.channel.send(f.math(msg))
 
-        if find_hi_words:  # hello
+        if msg == 'hello' or msg == 'hey' or msg=='hi':  # hello
           await message.channel.send(f'Привет {message.author.mention}! Я искуственный интелект. Я еще не совершенный поэтому вы не сможете писать мне что хотите. Чтобы узнать что я могу пропишите команду help.')
+
 
         if '/search' in msg:
           q = msg.replace('search','')
           wb.open('https://www.google.com/search?q='+q,new=2)
-        if '/weather' in msg:
+        if 'weather' in msg:
           city = msg.replace('weather','')
                               
           try:
@@ -62,7 +58,7 @@ class MyClient(discord.Client):
             observation = manager.weather_at_place(str(city))
           except:
             await message.channel.send('Город введен неверно.')
-          weather = observation.weather
+          weather = observation.weather()
           temp = weather.temperature("celsius").get("temp")
           temp_max = weather.temperature("celsius").get("temp_max")
           temp_min = weather.temperature("celsius").get("temp_min")
@@ -102,7 +98,6 @@ class MyClient(discord.Client):
           await message.channel.send(f.math_ran())
         if "/todo" in msg:
           await message.channel.send(f.todo(msg, author))
-
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
